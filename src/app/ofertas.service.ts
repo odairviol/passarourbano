b35cd0d79bb1faacd80d1c3d37ed49280c4d18d5
CASTBSB\odair.pereira <odair.pereira@castgroup.com.br>
2018-04-24 16:01:25 -0300

    Aulas 130

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -1,6 +1,7 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Oferta } from './shared/oferta.model';
+import { URL_API } from './app.api'
 
 import 'rxjs/add/operator/toPromise';
 
@@ -12,11 +13,24 @@ export class OfertasService {
 
     public getOfertas(): Promise<Oferta[]>{
         // efetuar uma requisição Http
-       return this.http.get('http://localhost:3000/ofertas?destaque=true')
+       return this.http.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json())
         // retornar uma promise Oferta[]
+    }
 
+    public getOfertasPorCategoria(categoria: string): Promise<Oferta[]>{
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
+        .toPromise()
+        .then((resposta: any) => resposta.json())
+    }
+
+    public getOfertaPorId(id: number): Promise<Oferta>{
+       return this.http.get(`${URL_API}?id=${id}`)
+        .toPromise()
+        .then((resposta: any) => {
+           return resposta.json()[0];
+        })
     }
 
     /*
