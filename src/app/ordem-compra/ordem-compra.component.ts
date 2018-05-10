0a41b79b898a7ea5cc9606bd3b17a257ea68375c
CASTBSB\odair.pereira <odair.pereira@castgroup.com.br>
2018-05-10 16:19:14 -0300

    Evloção passarourbano

--- src/app/ordem-compra/ordem-compra.component.ts
+++ src/app/ordem-compra/ordem-compra.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrdemCompraComponent implements OnInit {
 
+  public endereco: string = ''
+  public numero: string = ''
+  public complemento: string = ''
+  public formaPagamento: string = ''
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public atualizaEndereco(endereco: string): void {
+    this.endereco = endereco
+    console.log(this.endereco)
+  }
+
+  public atualizarNumero(numero: string): void {
+    this.numero = numero
+    console.log(this.numero)
+  }
+
+  public atualizaComplemento(complemento: string): void {
+    this.complemento = complemento
+    console.log(this.complemento)
+  }
+
+  public atualizaFormaPagamento(formaPagamento: string): void {
+    this.formaPagamento = formaPagamento
+    console.log(this.formaPagamento)
   }
 
 }
